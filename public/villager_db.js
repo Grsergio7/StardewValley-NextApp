50409aa412b6b7df0c975c72cd5805cd8f19a78b
Sergio Ruiz <62002026+Grsergio7@users.noreply.github.com>
2023-11-22 23:23:25 -0700

    villager info
    simple js database for villager's information

---
+++ public/villager_db.js
@@ -0,0 +1,192 @@
+export default {
+  villagers: [
+    {
+      id: "1",
+      url: "https://stardewvalleywiki.com/mediawiki/images/8/88/Abigail.png",
+      name: "Abigail",
+      Bday: "Fall 13",
+      Candidate: true,
+      Loves:
+        "All Universal Loves, Amethyst, Blackberry Cobbler, Chocolate Cake, Pufferfish, Pumpkin, Spicy Eel",
+      Likes: "All Universal Likes (except Vegetables), Quartz",
+      Neutral:
+        "All Universal Neutrals, All Milk, Daffodil, Dandelion, Ginger, Hazelnut, Leek, Snow Yam, Winter Root",
+      Dislikes:
+        "All Universal Dislikes (except Clay & Pufferfish), All Eggs, All Fruit (except Fruit Tree Fruit), All Vegetables (except Hops, Pumpkin, Tea Leaves, & Wheat), Sugar, Wild Horseradish",
+      Hates: "All Universal Hates (except Sugar), Clay, Holly",
+    },
+    {
+      id: "2",
+      url: "https://stardewvalleywiki.com/mediawiki/images/0/04/Alex.png",
+      name: "Alex",
+      Bday: "Summer 13",
+      Candidate: true,
+      Loves: "All Universal Loves, Complete Breakfast, Salmon Dinner",
+      Likes: "All Universal Likes, All Eggs (except Void Egg)",
+      Neutral:
+        "All Universal Neutrals, All Fruit (except Fruit Tree Fruit & Salmonberry), All Milk, Daffodil, Dandelion, Ginger, Hazelnut, Leek, Snow Yam, Winter Root",
+      Dislikes: "All Universal Dislikes, Salmonberry, Wild Horseradish",
+      Hates: "All Universal Hates, Holly, Quartz",
+    },
+    {
+      id: "3",
+      url: "https://stardewvalleywiki.com/mediawiki/images/b/bd/Elliott.png",
+      name: "Elliott",
+      Bday: "Fall 05",
+      Candidate: true,
+      Loves:
+        "All Universal Loves, Crab Cakes, Duck Feather, Lobster, Pomegranate, Squid Ink, Tom Kha Soup",
+      Likes:
+        "All Universal Likes (except Amaranth & Pizza), All Fruit (except Pomegranate & Salmonberry), Octopus, Squid",
+      Neutral:
+        "All Universal Neutrals (except Duck Feather & Squid Ink), All Eggs (except Void Egg), All Fish (except Carp, Lobster, Octopus, Sea Cucumber, Snail, & Squid)",
+      Dislikes:
+        "All Universal Dislikes (except Fish), All Milk, Daffodil, Dandelion, Ginger, Hazelnut, Leek, Holly, Pizza, Snow Yam, Wild Horseradish, Winter Root",
+      Hates: "All Universal Hates, Amaranth, Quartz, Salmonberry, Sea Cucumber",
+    },
+    {
+      id: "4",
+      url: "https://stardewvalleywiki.com/mediawiki/images/2/28/Emily.png",
+      name: "Emily",
+      Bday: "Spring 27",
+      Candidate: true,
+      Loves:
+        "All Universal Loves, Amethyst, Aquamarine, Cloth, Emerald, Jade, Ruby, Survival Burger, Topaz, Wool",
+      Likes:
+        "All Universal Likes (except Fish Taco, Fried Eel, Ice Cream, Maki Roll, Rice Pudding, Salmon Dinner, Spicy Eel, & Sashimi), Daffoldil, Quartz",
+      Neutral:
+        "All Universal Neutrals (except Wool), All Eggs (except Void Egg), All Fruit (except Fruit Tree Fruit & Salmonberry), All Milk, Dandelion, Ginger, Hazelnut, Leek, Snow Yam, Wild Horseradish, Winter Root, All Eggs, All Fruit, All Milk, All Mushrooms",
+      Dislikes:
+        "All Universal Dislikes, Fried Eel, Ice Cream, Rice Pudding, Salmonberry, Spicy Eel",
+      Hates:
+        "All Universal Hates, Fish Taco, Holly, Maki Roll, Salmon Dinner, Sashimi",
+    },
+    {
+      id: "5",
+      name: "Haley",
+      url: "https://stardewvalleywiki.com/mediawiki/images/1/1b/Haley.png",
+      Bday: "Spring 27",
+      Candidate: true,
+      Loves:
+        "All Universal Loves (except Prismatic Shard), Coconut, Fruit Salad, Pink Cake, Sunflower",
+      Likes: "All Universal Likes (except Vegetables), Daffoldil",
+      Neutral: "All Universal Neutrals",
+      Dislikes:
+        "All Universal Dislikes (except Clay & Fish), All Eggs, All Fruit (except Coconut), All Milk, All Vegetables (except Hops, Tea Leaves, & Wheat), Dandelion, Ginger, Hazelnut, Holly, Leek, Snow Yam, Winter Root, Quartz, All Eggs, All Fruit, All Milk, All Mushrooms, All Vegetables",
+      Hates:
+        "All Universal Hates, All Fish, Clay, Prismatic Shard, Wild Horseradish, All Fish",
+    },
+    {
+      id: "6",
+      name: "Harvey",
+      url: "https://stardewvalleywiki.com/mediawiki/images/9/95/Harvey.png",
+      Bday: "Spring 27",
+      Candidate: true,
+      Loves:
+        "All Universal Loves, Coffee, Pickles, Super Meal, Truffle Oil, Wine",
+      Likes:
+        "All Universal Likes (except Cheese, Goat Cheese, and the following cooked dishes: Blueberry Tart, Chocolate Cake, Cookie, Cranberry Sauce, Fried Mushroom, Glazed Yams, Hashbrowns, Ice Cream, Pancakes, Pink Cake, Pizza, Rhubarb Pie, & Rice Pudding), All Fruit (except Salmonberry & Spice Berry), Chanterelle, Common Mushroom, Daffodil, Duck Egg, Duck Feather, Dandelion, Ginger, Goat Milk, Hazelnut, Holly, Large Goat Milk, Leek, Magma Cap, Morel, Purple Mushroom, Quartz, Snow Yam, Spring Onion, Wild Horseradish, Winter Root",
+      Neutral:
+        "All Universal Neutrals (except Duck Feather, Bread, Coral, Nautilus Shell, & Rainbow Shell), All Eggs (except Duck Egg & Void Egg), Large Milk, Milk",
+      Dislikes:
+        "All Universal Dislikes (except Spring Onion), Blueberry Tart, Bread, Cheese, Chocolate Cake, Cookie, Cranberry Sauce, Fried Mushroom, Glazed Yams, Goat Cheese, Hashbrowns, Ice Cream, Pancakes, Pink Cake, Pizza, Rhubarb Pie, Rice Pudding",
+      Hates:
+        "All Universal Hates, Coral, Nautilus Shell, Rainbow Shell, Salmonberry, Spice Berry",
+    },
+    {
+      id: "7",
+      url: "https://stardewvalleywiki.com/mediawiki/images/e/e6/Leah.png",
+      name: "Leah",
+      Bday: "Winter 23",
+      Candidate: true,
+      Loves:
+        "All Universal Loves, Goat Cheese, Poppyseed Muffin, Salad, Stir Fry, Truffle, Vegetable Medly, Wine",
+      Likes:
+        "All Universal Likes (except: Foraged Minerals other than Earth Crystal, Gems other than Diamond & Prismatic Shard, Carp Surprise, Cookie, Hashbrowns, Ice Cream, Pancakes, Pink Cake, Pizza, Rice Pudding, Survival Burger, & Tortilla), All Eggs (except: Void Egg), All Milk, All Fruit, All Mushrooms, Daffoldil, Dandelion, Ginger, Hazelnut, Holly, Leek, Snow Yam, Winter Root, spring onion, Wild Horseradish",
+      Neutral: "All Universal Neutrals (except Bread, Fried Egg, & Truffle)",
+      Dislikes:
+        "All Universal Dislikes (except Driftwood, Spring Onion, & Void Egg), All Foraged Minerals (except Earth Crystal), All Gems (except Diamond & Prismatic Shard), Carp Surprise, Cookies, Fried Egg, Ice Cream, Pink Cake, Rice Pudding, Seaweed, Survival Burger, Tortilla",
+      Hates:
+        "All Universal Hates (except Seaweed), Bread, Hashbrowns, Pancakes, Pizza, Void Egg",
+    },
+    {
+      id: "8",
+      url: "https://stardewvalleywiki.com/mediawiki/images/f/f8/Maru.png",
+      name: "Maru",
+      Bday: "Summer 10",
+      Candidate: true,
+      Loves:
+        "All Universal Loves, Battery Pack, Cauliflower, Chesse Cauliflower, Diamond, Gold Bar, Iridium Bar, Miner's Treat, Pepper Poppers, Radioactive Bar, Rhubarb Pie, Strawberry",
+      Likes:
+        "All Universal Likes (except Honey, Maple Syrup, & Pickles), Copper Bar, Iron Bar, Oak Resin, Pine Tar, Quartz, Radioactive Ore",
+      Neutral:
+        "All Universal Neutrals (except Truffle), All Eggs (except Void Egg), All Fruit (except Blackberry, Crystal Fruit, Fruit Tree Fruit, Salmonberry, & Strawberry), All Milk, Daffoldil, Dandelion, Ginger, Hazelnut, Leek, Wild Horseradish, Winter Root, All Eggs, All Fruit, All Milk, All Mushrooms",
+      Dislikes:
+        "All Universal Dislikes (except Battery Pack, Copper Bar, Gold Bar, Iridium Bar, Iron Bar, Oak Resin, & Pine Tar), Blackberry, Common Mushroom, Crystal Fruit, Maple Syrup, Salmonberry",
+      Hates:
+        "All Universal Hates (except Radioactive Bar & Radioactive Ore), Holly, Honey, Pickles, Snow Yam, Truffle",
+    },
+    {
+      id: "9",
+      name: "Penny",
+      url: "https://stardewvalleywiki.com/mediawiki/images/a/ab/Penny.png",
+      Bday: "Fall 02",
+      Candidate: true,
+      Loves:
+        "All Universal Loves (except Rabbit's Foot), Diamond, Emerald, Melon, Poppy, Poppyseed Muffin, Red Plate, Roots Platter, Sandfish, Tom Kha Soup",
+      Likes:
+        "All Universal Likes (except Algae Soup, Beer, Mead, Pale Ale, Pale Broth, Piña Colada, Tom Kha Soup, & Wine), All Milk, All Artifacts, Dandelion, Leek, All Artifacts, All Milk",
+      Neutral:
+        "All Universal Neutrals (except Duck Feather, Hops, & Wool), All Eggs (except Void Egg), All Fruit (except Fruit Tree Fruit, Grape, Melon, & Salmonberry), Chanterelle, Common Mushroom, Daffoldil, Ginger, Hazelnut, Snow Yam, Wild Horseradish, Winter Root",
+      Dislikes:
+        "All Universal Dislikes (except Artifacts and Sandfish), Algae Soup, Duck Feather, Pale Broth, Purple Mushroom, Quartz, Red Mushroom, Salmonberry, Wool",
+      Hates:
+        "All Universal Hates (except Poppy and Red Mushroom), Beer, Mead, Wine, Grape, Holly, Hops, Pale Ale, Piña Colada, Rabbit's Foot",
+    },
+    {
+      id: "10",
+      name: "Sam",
+      url: "https://stardewvalleywiki.com/mediawiki/images/9/94/Sam.png",
+      Bday: "Summer 17",
+      Candidate: true,
+      Loves: "All Universal Loves, Cactus Fruit, Maple Bar, Pizza, Tigerseye",
+      Likes:
+        "All Universal Likes (except Duck Mayonnaise, Mayonnaise, Pickles, & Vegetables), All Eggs (except Void Egg), Joja Cola",
+      Neutral:
+        "All Universal Neutrals, All Fruit (except Cactus Fruit, Fruit Tree Fruit, & Salmonberry), All Milk",
+      Dislikes:
+        "All Universal Dislikes (except Coal, Copper Bars, Gold Bars, Gold Ore, Iridium Bars, Iridium Ore, Iron Bars, Refined Quartz, & Tigerseye), All Vegetables (except Hops, Tea Leaves, & Wheat), Chanterelle, Common Mushroom, Daffodil, Dandelion, Ginger, Hazelnut, Holly, Leek, Magma Cap, Morel, Purple Mushroom, Quartz, Snow Yam, Salmonberry, Seaweed, Wild Horseradish, Winter Root",
+      Hates:
+        "All Universal Hates (except Joja Cola & Seaweed), Coal, Copper Bar, Duck Mayo, Gold Bar, Gold Ore, Iridium Bar, Iridium Ore, Iron Bar, Mayo, Pickles, Refined Quartz",
+    },
+    {
+      id: "11",
+      name: "Sebastian",
+      url: "https://stardewvalleywiki.com/mediawiki/images/a/a8/Sebastian.png",
+      Bday: "Winter 10",
+      Candidate: true,
+      Loves: "Frozen Tear, Obsidian, Pumpkin Soup, Sashimi, Void Egg",
+      Likes:
+        "All Universal Likes (except Flowers, Complete Breakfast, Farmer's Lunch, Omelet, and Artisan Goods other than Coffee, Green Tea, and Oil)",
+      Neutral: "Flounder, Quartz",
+      Dislikes:
+        "All Universal Neutrals, All Fruit (except Fruit Tree Fruit & Salmonberry), All Fish (except Carp, Flounder, & Snail), All Milk, Chanterelle, Common Mushroom, Daffodil, Dandelion, Ginger, Hazelnut, Holly, Leek, Magma Cap, Morel, Purple Mushroom, Salmonberry, Snow Yam, Wild Horseradish, Winter Root",
+      Hates:
+        "All Universal Hates, All Artisan Goods (except Coffee, Green Tea, & Oil), All Eggs (except Void Egg), Clay, Complete Breakfast, Farmer's Lunch, Omelet, Piña Colada",
+    },
+    {
+      id: "12",
+      name: "Shane",
+      url: "https://stardewvalleywiki.com/mediawiki/images/8/8b/Shane.png",
+      Bday: "Spring 20",
+      Candidate: true,
+      Loves: "Beer, Hot Pepper, Pepper Poppers, Pizza",
+      Likes:
+        "All Universal Likes (Except Pickles), All Fruits(Except Hot Pepper), All Eggs(Except Void Egg)",
+      Neutral: "All Universal Neutrals, All Milks",
+      Dislikes:
+        "All Universal Dislikes, Chanterelle, Common Mushroom, Daffodil, Dandelion, Ginger, Hazelnut, Holly, Leek, Magma Cap, Morel, Purple Mushroom, Seaweed, Snow Yam, Wild Horseradish, Winter Root",
+      Hates: "Pickles, Quartz, All Unviversal Hates (Except Seaweed)",
+    },
+  ],
+};
